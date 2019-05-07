a53c45c7d5e9709086cd89d38d7b07130946887a
CHQ <111>
2019-05-07 21:25:55 +0800

    000

--- src/main/resources/static/layuiadmin/modules/useradmin.js
+++ src/main/resources/static/layuiadmin/modules/useradmin.js
@@ -17,7 +17,7 @@ layui.define(['table', 'form'], function (exports) {
         , url: 'getListByPage' //模拟接口
         , cols: [[
             {type: 'checkbox', fixed: 'left'}
-            , {field: 'id', width: 100, title: 'ID', sort: true}
+            , {field: 'id', width: 80, title: 'ID', sort: true}
             , {field: 'realName', title: '姓名'}
             , {field: 'username', title: '账号'}
             , {field: 'userType', title: '类型', templet: '#userTypeTpl'}
@@ -46,16 +46,24 @@ layui.define(['table', 'form'], function (exports) {
     table.on('tool(LAY-user-manage)', function (obj) {
         var data = obj.data;
         if (obj.event === 'del') {
-            layer.prompt({
-                formType: 1
-                , title: '敏感操作，请验证口令'
-            }, function (value, index) {
-                layer.close(index);
-
-                layer.confirm('真的删除行么', function (index) {
-                    obj.del();
-                    layer.close(index);
+            layer.confirm('确定删除此用户？', function (index) {
+                $.ajax({
+                    url: "delete",
+                    type: "post",
+                    data: {
+                        id: data.id
+                    },
+                    success: function (result) {
+                        if (result.success) {
+                            table.reload('LAY-user-manage'); //数据刷新
+                            layer.close(index); //关闭弹层
+                            layer.msg('删除成功');
+                        } else {
+                            layer.msg('删除失败');
+                        }
+                    }
                 });
+                layer.close(index);
             });
         } else if (obj.event === 'edit') {
             var tr = $(obj.tr);
@@ -150,72 +158,6 @@ layui.define(['table', 'form'], function (exports) {
 
                 }
             });
-        }
-    });
-
-    //管理员管理
-    table.render({
-        elem: '#LAY-user-back-manage'
-        , url: layui.setter.base + 'json/useradmin/mangadmin.js' //模拟接口
-        , cols: [[
-            {type: 'checkbox', fixed: 'left'}
-            , {field: 'id', width: 80, title: 'ID', sort: true}
-            , {field: 'loginname', title: '登录名'}
-            , {field: 'telphone', title: '手机'}
-            , {field: 'email', title: '邮箱'}
-            , {field: 'role', title: '角色'}
-            , {field: 'jointime', title: '加入时间', sort: true}
-            , {field: 'check', title: '审核状态', templet: '#buttonTpl', minWidth: 80, align: 'center'}
-            , {title: '操作', width: 150, align: 'center', fixed: 'right', toolbar: '#table-useradmin-admin'}
-        ]]
-        , text: '对不起，加载出现异常！'
-    });
-
-    //监听工具条
-    table.on('tool(LAY-user-back-manage)', function (obj) {
-        var data = obj.data;
-        if (obj.event === 'del') {
-            layer.prompt({
-                formType: 1
-                , title: '敏感操作，请验证口令'
-            }, function (value, index) {
-                layer.close(index);
-                layer.confirm('确定删除此管理员？', function (index) {
-                    console.log(obj)
-                    obj.del();
-                    layer.close(index);
-                });
-            });
-        } else if (obj.event === 'edit') {
-            var tr = $(obj.tr);
-
-            layer.open({
-                type: 2
-                , title: '编辑管理员'
-                , content: '../../../views/user/administrators/adminform.html'
-                , area: ['420px', '420px']
-                , btn: ['确定', '取消']
-                , yes: function (index, layero) {
-                    var iframeWindow = window['layui-layer-iframe' + index]
-                        , submitID = 'LAY-user-back-submit'
-                        , submit = layero.find('iframe').contents().find('#' + submitID);
-
-                    //监听提交
-                    iframeWindow.layui.form.on('submit(' + submitID + ')', function (data) {
-                        var field = data.field; //获取提交的字段
-
-                        //提交 Ajax 成功后，静态更新表格中的数据
-                        //$.ajax({});
-                        table.reload('LAY-user-front-submit'); //数据刷新
-                        layer.close(index); //关闭弹层
-                    });
-
-                    submit.trigger('click');
-                }
-                , success: function (layero, index) {
-
-                }
-            })
         }
     });
 
